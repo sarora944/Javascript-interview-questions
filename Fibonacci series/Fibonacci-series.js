2a1188055987f77ef62c4676e11b7dffc4faaa52
Sarora29944 <sarora29944@gmail.com>
2024-05-23 14:29:40 +0530

    interview topics

--- Fibonacci series/Fibonacci-series.js
+++ Fibonacci series/Fibonacci-series.js
@@ -1,16 +1,16 @@
 // Solution 1
-const fibonacci = (num)=>{
-    const arr = [0];
-    for (let i = 0; i < num-1; i++) {
-        if(arr.length<2){
-            arr.push(i+1)
-        }else{
-            const newNum = arr[arr.length-1] + arr[arr.length-2]
-            arr.push(newNum)
-        }
-    }
-    return arr
-}
+// const fibonacci = (num)=>{
+//     const arr = [0];
+//     for (let i = 0; i < num-1; i++) {
+//         if(arr.length<2){
+//             arr.push(i+1)
+//         }else{
+//             const newNum = arr[arr.length-1] + arr[arr.length-2]
+//             arr.push(newNum)
+//         }
+//     }
+//     return arr
+// }
 
 // Solution 2
 // const fibonacci = (num)=>{
@@ -22,6 +22,19 @@ const fibonacci = (num)=>{
 //         num2 = nextNum
 //     }
 // }
+
+// Solution 3
+let arr = [0,1]
+const fibonacci = (num, initialNum = 0)=>{
+    if(initialNum<num){
+        const a = arr[arr.length-1];
+        const b = arr[arr.length-2];
+        arr.push(a+b)
+        initialNum++
+        return fib(num, initialNum);
+    }
+    console.log(arr)
+}
 const series = fibonacci(12);
 
 console.log(series)
